cee8e760c1b36af441e7894f5f1b6a6ecdd9e27a
nomulavenkatramana <87078767+nomulavenkatramana@users.noreply.github.com>
2022-05-05 19:12:35 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
+function App(props) {
+  const [counter,setCounter] =useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <p>This is para testing</p>
+      <div id="test">welcome to {props.name}</div>
+      <div id="init">{counter}</div>
+      <button onClick={()=> setCounter(counter+1)}>increment</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme,{shallow} from 'enzyme';
+import Adaptor from '@cfaester/enzyme-adapter-react-18';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter:new Adaptor() });
+
+test("checking h1 content",()=>{
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('h1').text()).toContain('Hello World!');
+})
+
+  test("checking para content",()=>{
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('p').text()).toContain('This is para testing');
+
+})
+
+test("checking props",()=>{
+  const wrapper = shallow(<App name="React" />);
+  expect(wrapper.find('#test').text()).toBe('welcome to React');
+})
+
+test("checking state initial value",()=>{
+  const wrapper = shallow(<App name="React" />);
+  expect(wrapper.find('#init').text()).toBe('0');
+})
+
+test("checking onclick",()=>{
+  const wrapper = shallow(<App />);
+  wrapper.find('button').simulate('click');
+  expect(wrapper.find('#init').text()).toBe('1');
+})
+
